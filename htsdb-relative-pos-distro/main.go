f89a124eab5ef79a93c93b7a305c3b30f3f60d4f
Emmanouil "Manolis" Maragkakis <mns.marag@gmail.com>
2017-02-03 17:07:18 -0500

    htsdb-relative-pos-distro: rewrite to fix concurrency
    With the old design if reference sequences where more than 100000 the
    program would deadlock.

--- htsdb-relative-pos-distro/main.go
+++ htsdb-relative-pos-distro/main.go
@@ -3,83 +3,142 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
+	"sync"
 
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/Masterminds/squirrel"
+	"github.com/alexflint/go-arg"
 	"github.com/biogo/biogo/feat"
 	"github.com/jmoiron/sqlx"
 	"github.com/mnsmar/htsdb"
-	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-const prog = "htsdb-relative-pos-distro"
-const version = "0.5"
-const descr = `Measure the distribution of the relative position of reads in
-database 1 against reads in database 2. For each relative position in the
-provided span, print the number of read pairs with this relative positioning
-in the two databases, the total number of possible pairs and the total number
-of reads in each database. Read relative position is measured either 5'-5' or
-3'-3'. Positive numbers indicate read 1 is downstream of read 2. Supports
-grouping by reference. Provided SQL filters will apply to all counts.`
-const maxConc = 512
+const maxConc = 12
 
-var (
-	app = kingpin.New(prog, descr)
-
-	dbFile1 = app.Flag("db1", "SQLite file for database 1.").
-		PlaceHolder("<file>").Required().String()
-	tab1 = app.Flag("table1", "Database table name for db1.").
-		Default("sample").String()
-	where1 = app.Flag("where1", "SQL filter injected in WHERE clause for db1.").
-		PlaceHolder("<SQL>").String()
-	dbFile2 = app.Flag("db2", "SQLite file for database 2.").
-		PlaceHolder("<file>").Required().String()
-	tab2 = app.Flag("table2", "Database table name for db2.").
-		Default("sample").String()
-	where2 = app.Flag("where2", "SQL filter injected in WHERE clause for db2.").
-		PlaceHolder("<SQL>").String()
-	from1 = app.Flag("pos1", "Reference point for relative position measurement.").
-		Required().PlaceHolder("<5p|3p>").Enum("5p", "3p")
-	from2 = app.Flag("pos2", "Reference point for relative position measurement.").
-		Required().PlaceHolder("<5p|3p>").Enum("5p", "3p")
-	anti = app.Flag("anti", "Compare reads on opposite instead of same orientation.").
-		Bool()
-	span = app.Flag("span", "Maximum distance between compared reads.").
-		Default("100").PlaceHolder("<int>").Int()
-	groupByChrom = app.Flag("by-ref", "Group counts by reference.").
-			Bool()
-	collapse1 = app.Flag("collapse1", "Collapse the reads that have the same pos1.").
-			Bool()
-	collapse2 = app.Flag("collapse2", "Collapse the reads that have the same pos2.").
-			Bool()
-	verbose = app.Flag("verbose", "Verbose mode.").Short('v').Bool()
-)
-
-type job struct {
-	verbose      bool
-	ref          htsdb.Reference
-	decs1, decs2 []BuilderDecorator
-	db1, db2     *sqlx.DB
-	span         int
-	getPos1      func(feat.Range, feat.Orientation) int
-	getPos2      func(feat.Range, feat.Orientation) int
-	collapse1    bool
-	collapse2    bool
+// Opts is the struct with the options that the program accepts.
+type Opts struct {
+	DB1       string `arg:"required,help:SQLite3 database 1"`
+	Table1    string `arg:"required,help:table name for db1"`
+	Where1    string `arg:"help:SQL filter injected in WHERE clause of db1"`
+	Pos1      string `arg:"required,help:reference point for reads of db1; one of 5p or 3p"`
+	Collapse1 bool   `arg:"help:Collapse reads that have the same pos1"`
+	DB2       string `arg:"required,help:SQLite3 database 2"`
+	Table2    string `arg:"required,help:table name for db2"`
+	Where2    string `arg:"help:SQL filter injected in WHERE clause of db2"`
+	Pos2      string `arg:"required,help:reference point for reads of db2; one of 5p or 3p"`
+	Collapse2 bool   `arg:"help:collapse reads that have the same pos2"`
+	Span      int    `arg:"required,help:maximum distance of compared pos"`
+	GroupRef  bool   `arg:"--by-ref,help:group counts by reference"`
+	Anti      bool   `arg:"help:Compare reads on opposite instead of same orientation"`
+	Verbose   bool   `arg:"-v,help:report progress"`
 }
 
-type result struct {
-	hist   map[int]uint
-	count1 int
-	count2 int
-	job    job
+// Version returns the program version.
+func (Opts) Version() string {
+	return "htsdb-relative-pos-distro 0.6"
+}
+
+// Description returns an extended description of the program.
+func (Opts) Description() string {
+	return "Measure distribution of read relative positions in database 1 against database 2. Prints the number of read pairs at each relative position along with the total number of possible pairs and the total number of reads in each database. Positive relative positions indicate read 1 is downstream of read 2. Provided SQL filters will apply to all counts."
+}
+
+func main() {
+	var err error
+	var opts Opts
+	var db1, db2 *sqlx.DB
+
+	p := arg.MustParse(&opts)
+	if opts.Pos1 != "5p" && opts.Pos1 != "3p" {
+		p.Fail("--pos1 must be either 5p or 3p")
+	}
+	if opts.Pos2 != "5p" && opts.Pos2 != "3p" {
+		p.Fail("--pos2 must be either 5p or 3p")
+	}
+
+	// open database connections.
+	if db1, err = sqlx.Connect("sqlite3", opts.DB1); err != nil {
+		log.Fatal(err)
+	}
+	if db2, err = sqlx.Connect("sqlite3", opts.DB2); err != nil {
+		log.Fatal(err)
+	}
+
+	// create select decorators.
+	decors1 := []BuilderDecorator{Table(opts.Table1), Where(opts.Where1)}
+	decors2 := []BuilderDecorator{Table(opts.Table2), Where(opts.Where2)}
+
+	// extract reference features
+	refs, err := readRefs(db1, db2, decors1, decors2)
+	if err != nil {
+		log.Fatal("error reading BED:", err)
+	}
+
+	// goroutine that sends each reference as a job to jobs.
+	jobs := make(chan job)
+	go func() {
+		for _, ref := range refs {
+			jobs <- job{
+				opts:    opts,
+				ref:     ref,
+				db1:     db1,
+				db2:     db2,
+				decors1: decors1,
+				decors2: decors2,
+			}
+		}
+		close(jobs)
+	}()
+
+	// start workers that consume jobs and send results to results.
+	results := make(chan result)
+	var wg sync.WaitGroup
+	wg.Add(maxConc)
+	for w := 1; w <= maxConc; w++ {
+		go func() {
+			worker(w, jobs, results)
+			wg.Done()
+		}()
+	}
+
+	// goroutine that checks when all workers are done and closes results.
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	// print output
+	if opts.GroupRef == true {
+		fmt.Printf("ref\tpos\tpairs\treadCount1\treadCount2\n")
+		for res := range results {
+			for i := -opts.Span; i <= opts.Span; i++ {
+				fmt.Printf("%s\t%d\t%d\t%d\t%d\n",
+					res.job.ref.Name(), i, res.hist[i], res.count1, res.count2)
+			}
+		}
+	} else {
+		var totalCount1, totalCount2 int
+		aggrHist := make(map[int]uint)
+		for res := range results {
+			for k, v := range res.hist {
+				aggrHist[k] += v
+			}
+			totalCount1 += res.count1
+			totalCount2 += res.count2
+		}
+
+		fmt.Printf("pos\tpairs\treadCount1\treadCount2\n")
+		for i := -opts.Span; i <= opts.Span; i++ {
+			fmt.Printf("%d\t%d\t%d\t%d\n", i, aggrHist[i], totalCount1, totalCount2)
+		}
+	}
 }
 
 func worker(id int, jobs <-chan job, results chan<- result) {
 	for j := range jobs {
-		if j.verbose == true {
-			log.Printf("wID:%d, chrom:%s\n", id, j.ref.Chrom)
+		if j.opts.Verbose == true {
+			log.Printf("wID:%d, chrom:%s\n", id, j.ref.Name())
 		}
 
 		var err error
@@ -87,8 +146,8 @@ func worker(id int, jobs <-chan job, results chan<- result) {
 
 		// assemble sqlx select builders
 		rangeDec := Where("strand = ? AND rname = ?")
-		readsB1 := DecorateBuilder(htsdb.RangeBuilder, append(j.decs1, rangeDec)...)
-		readsB2 := DecorateBuilder(htsdb.RangeBuilder, append(j.decs2, rangeDec)...)
+		readsB1 := DecorateBuilder(htsdb.RangeBuilder, append(j.decors1, rangeDec)...)
+		readsB2 := DecorateBuilder(htsdb.RangeBuilder, append(j.decors2, rangeDec)...)
 
 		// prepare statements.
 		var readsStmt1, readsStmt2 *sqlx.Stmt
@@ -98,6 +157,15 @@ func worker(id int, jobs <-chan job, results chan<- result) {
 		if readsStmt2, err = prepareStmt(readsB2, j.db2); err != nil {
 			log.Fatal(err)
 		}
+		// get position extracting function
+		getPos1 := htsdb.Head
+		if j.opts.Pos1 == "3p" {
+			getPos1 = htsdb.Tail
+		}
+		getPos2 := htsdb.Head
+		if j.opts.Pos2 == "3p" {
+			getPos2 = htsdb.Tail
+		}
 
 		hist := make(map[int]uint)
 		var count1, count2 int
@@ -105,10 +173,10 @@ func worker(id int, jobs <-chan job, results chan<- result) {
 			// loop on reads in db1.
 			wig := make(map[int]uint)
 			ori1 := ori
-			if *anti == true {
+			if j.opts.Anti == true {
 				ori1 = -1 * ori1
 			}
-			rows1, err := readsStmt1.Queryx(ori1, j.ref.Chrom)
+			rows1, err := readsStmt1.Queryx(ori1, j.ref.Name())
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -116,8 +184,8 @@ func worker(id int, jobs <-chan job, results chan<- result) {
 				if err = rows1.StructScan(&r); err != nil {
 					log.Fatal(err)
 				}
-				pos := j.getPos1(&r, ori1)
-				if _, ok := wig[pos]; ok && j.collapse1 {
+				pos := getPos1(&r, ori1)
+				if _, ok := wig[pos]; ok && j.opts.Collapse1 {
 					continue
 				}
 				count1++
@@ -126,7 +194,7 @@ func worker(id int, jobs <-chan job, results chan<- result) {
 
 			// loop on reads in db2.
 			visited := make(map[int]bool)
-			rows2, err := readsStmt2.Queryx(ori, j.ref.Chrom)
+			rows2, err := readsStmt2.Queryx(ori, j.ref.Name())
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -134,13 +202,13 @@ func worker(id int, jobs <-chan job, results chan<- result) {
 				if err = rows2.StructScan(&r); err != nil {
 					log.Fatal(err)
 				}
-				pos := j.getPos2(&r, ori)
-				if visited[pos] && j.collapse2 {
+				pos := getPos2(&r, ori)
+				if visited[pos] && j.opts.Collapse2 {
 					continue
 				}
 				visited[pos] = true
 				count2++
-				for relPos := -j.span; relPos <= j.span; relPos++ {
+				for relPos := -j.opts.Span; relPos <= j.opts.Span; relPos++ {
 					if pos+relPos < 0 {
 						continue
 					}
@@ -154,106 +222,48 @@ func worker(id int, jobs <-chan job, results chan<- result) {
 	}
 }
 
-func main() {
-	app.HelpFlag.Short('h')
-	app.Version(version)
-	_, err := app.Parse(os.Args[1:])
-	if err != nil {
-		kingpin.Fatalf("%s", err)
-	}
+func readRefs(
+	db1, db2 *sqlx.DB, decors1, decors2 []BuilderDecorator) ([]feat.Feature, error) {
 
-	// open database connections.
-	db1 := connectDB(*dbFile1)
-	db2 := connectDB(*dbFile2)
-
-	// the decorators that apply required SQL clauses on each connection.
-	decs1 := []BuilderDecorator{Table(*tab1), Where(*where1)}
-	decs2 := []BuilderDecorator{Table(*tab2), Where(*where2)}
+	var refs []feat.Feature
 
 	// select reference features
-	refsB1 := DecorateBuilder(htsdb.ReferenceBuilder, decs1...)
+	refsB1 := DecorateBuilder(htsdb.ReferenceBuilder, decors1...)
 	refs1, err := htsdb.SelectReferences(db1, refsB1)
 	if err != nil {
 		log.Fatal(err)
 	}
-	refsB2 := DecorateBuilder(htsdb.ReferenceBuilder, decs2...)
+	refsB2 := DecorateBuilder(htsdb.ReferenceBuilder, decors2...)
 	refs2, err := htsdb.SelectReferences(db2, refsB2)
 	if err != nil {
 		log.Fatal(err)
 	}
-	refs := make(map[string]htsdb.Reference)
+
+	refsmap := make(map[string]htsdb.Reference)
 	for _, r := range append(refs1, refs2...) {
-		refs[r.Chrom] = r
+		refsmap[r.Chrom] = r
 	}
 
-	// get position extracting function
-	getPos1 := htsdb.Head
-	if *from1 == "3p" {
-		getPos1 = htsdb.Tail
-	}
-	getPos2 := htsdb.Head
-	if *from2 == "3p" {
-		getPos2 = htsdb.Tail
+	for k := range refsmap {
+		f := refsmap[k]
+		refs = append(refs, &f)
 	}
 
-	// deploy workers
-	jobs := make(chan job, 100000)
-	results := make(chan result, 100)
-	for w := 1; w <= maxConc; w++ {
-		go worker(w, jobs, results)
-	}
+	return refs, nil
+}
 
-	// create the jobs
-	jobCnt := 0
-	for _, ref := range refs {
-		jobCnt++
-		jobs <- job{
-			verbose:   *verbose,
-			ref:       ref,
-			db1:       db1,
-			db2:       db2,
-			decs1:     decs1,
-			decs2:     decs2,
-			span:      *span,
-			getPos1:   getPos1,
-			getPos2:   getPos2,
-			collapse1: *collapse1,
-			collapse2: *collapse2,
-		}
-	}
-	close(jobs)
+type job struct {
+	opts             Opts
+	ref              feat.Feature
+	decors1, decors2 []BuilderDecorator
+	db1, db2         *sqlx.DB
+}
 
-	// collect results for all jobs in a slice
-	var allResults []result
-	for a := 0; a < jobCnt; a++ {
-		allResults = append(allResults, <-results)
-	}
-
-	// print output
-	if *groupByChrom == true {
-		fmt.Printf("ref\tpos\tpairs\treadCount1\treadCount2\n")
-		for _, res := range allResults {
-			for i := -*span; i <= *span; i++ {
-				fmt.Printf("%s\t%d\t%d\t%d\t%d\n",
-					res.job.ref.Name(), i, res.hist[i], res.count1, res.count2)
-			}
-		}
-	} else {
-		var totalCount1, totalCount2 int
-		aggrHist := make(map[int]uint)
-		for _, res := range allResults {
-			for k, v := range res.hist {
-				aggrHist[k] += v
-			}
-			totalCount1 += res.count1
-			totalCount2 += res.count2
-		}
-
-		fmt.Printf("pos\tpairs\treadCount1\treadCount2\n")
-		for i := -*span; i <= *span; i++ {
-			fmt.Printf("%d\t%d\t%d\t%d\n", i, aggrHist[i], totalCount1, totalCount2)
-		}
-	}
+type result struct {
+	hist   map[int]uint
+	count1 int
+	count2 int
+	job    job
 }
 
 // A BuilderDecorator wraps a squirrel.SelectBuilder with extra behaviour.
@@ -299,12 +309,4 @@ func prepareStmt(b squirrel.SelectBuilder, db *sqlx.DB) (*sqlx.Stmt, error) {
 		return nil, err
 	}
 	return stmt, nil
-}
-
-func connectDB(file string) *sqlx.DB {
-	db, err := sqlx.Connect("sqlite3", file)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return db
 }
